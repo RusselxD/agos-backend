82ba735008771cb66c28db145d079ef22db54c24
PSergio984 <ericgabrielmanabat@plv.edu.ph>
2026-02-17 17:55:07 +0800

    feat: update test setup and add post endpoint tests,also added loggers in requirements.txt
     Conflicts:

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,5 +1,5 @@
 from typing import AsyncGenerator, Generator
-
+import os
 import pytest
 from httpx import AsyncClient
 from fastapi.testclient import TestClient
@@ -7,8 +7,10 @@ from fastapi.testclient import TestClient
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-from app.database import Base
-from app.main import app, get_session
+os.environ["ENV_STATE"] = "test"
+
+from app.core.database import database
+from app.main import app
 
 
 @pytest.fixture(scope="session")
@@ -23,35 +25,16 @@ def client(db) -> Generator:
         yield c
 
 
-# This fixture sets up an in-memory SQLite database for testing
-# and overrides the get_session dependency to use this test database.
+#
 @pytest.fixture(autouse=True)
 async def db() -> AsyncGenerator:
-
-    # Create an in-memory SQLite database for testing
-    engine = create_async_engine("sqlite+aiosqlite:///:memory:", echo=True)
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-    # Create a session factory
-    async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
-
-    # Override the get_session dependency to use the test database
-    async def override_get_session() -> AsyncGenerator:
-        async with async_session() as session:
-            yield session
-
-    app.dependency_overrides[get_session] = override_get_session
-
+    await database.connect()
     yield
-
-    # Cleanup: Drop all tables after tests
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
+    await database.disconnect()
 
 
 # This fixture provides an AsyncClient for testing the FastAPI.
 @pytest.fixture()
-async def async_client() -> AsyncGenerator:
-    async with AsyncClient(app=app, base_url=client.base_url) as ac:
+async def async_client(db) -> AsyncGenerator:
+    async with AsyncClient(app=app, base_url="http://testserver") as ac:
         yield ac
