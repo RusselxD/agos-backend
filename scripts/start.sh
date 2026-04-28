07682676a5f5b110b9f53b2b5b8a364b3b99fddf
RusselxD <russelcabigquez8@gmail.com>
2026-04-28 10:27:37 +0800

    fixed mixed-content error:

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,4 +8,4 @@ alembic upgrade head
 python scripts/seed_db.py
 
 # Start app
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --proxy-headers --forwarded-allow-ips="*"
